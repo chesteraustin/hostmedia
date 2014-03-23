33d9738e7972fc93a43c3214df8fb099a4e9aaa8
OpenShift guest <shermaineabad@gmail.com>
2014-03-23 14:28:19 -0400

    resync c9.io

---
+++ lego/test.sql
@@ -0,0 +1,8 @@
+  INSERT INTO entry (
+  Set_Name,
+  Set_Number, 
+  Set_Theme, 
+  Purchase_Price, 
+  Purchase_From, 
+  Purchase_Date, 
+  Status, Sell_Price, Sell_From, Sell_Date ) VALUES ('Sacha White', '86', 'Laborum enim ut est labore exercitation omnis autem alias nobis nihil dolore eligendi aut voluptatem veniam et', '', 'Et in culpa consequatur id omnis ea commodo', '1993-09-23', 'New', '', 'Natus similique eu sint in voluptates laboris aute consequatur Qui in elit quis rerum', '84,1990-07-22');
