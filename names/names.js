5f027b5a1420405585edc39a7188eb014a9135c4
chesteraustin <chesteraustin@gmail.com>
2014-08-12 09:59:49 -0700

    Initial test

--- names/names.js
+++ names/names.js
@@ -1,21 +1,84 @@
 $(document).ready(function () { 
-	addName();
+	dialog = $("#newNameModal").dialog({
+		autoOpen: false,
+		height: 300,
+		width: 350,
+		modal: true,
+		buttons: {
+			"Add Name": function() {
+				addName();
+				getNames();
+			},
+			Close: function() {
+				dialog.dialog("close");
+			}
+		},
+		close: function() {
+//			form[0].reset();
+//			allFields.removeClass( "ui-state-error" );
+		}
+	});
+
+	$("#newNameBtn").button().on( "click", function() {
+		dialog.dialog( "open" );
+	});
+
+	$("#clearNamesBtn").button().on( "click", function() {
+		clearNames();
+	});
+
+	$("#saveNamesBtn").button().on( "click", function() {
+		saveNames();
+	});
+
+	getNames();
+	$("#listNames").sortable();
 	console.log("document ready");
 });
 
-function addName(){
-	var name = "jsName";
-	var position = 7;
-	var gender = "jsGen";
+function getNames(){
+	//Clear all entries
+	$("#listNames").empty();
+
+	var gender = "male";
 
 	$.ajax({
 		type: 'get', 
 		url: 'http://www.chesteraustin.us/cfc/TPNames.cfc?ReturnFormat=json',  
 		data: {
-			'method': 'clearGender',
-			'Name': name,
-			'Position': position,
+			'method': 'getNames',
 			'Gender': gender
+			},
+		contentType: 'json',
+		dataType: 'json',
+		success: function(response) {
+			console.log(response);
+			for(var i=0; i < response.DATA.length; i++) {
+				var name = response.DATA[i][1];
+				var newItem = document.createElement("li");
+				newItem.className = "ui-state-default";
+				newItem.innerHTML = name;
+
+//				var addID = newItem.innerText(name);
+				$("#listNames").append(newItem);
+			};
+		}
+	});
+}
+
+function addName(){
+	var newName = $("#newName").val();
+	var gender = "male";
+	var currentPosition = $("#listNames li").length + 1;
+
+	$.ajax({
+		type: 'get', 
+		url: 'http://www.chesteraustin.us/cfc/TPNames.cfc?ReturnFormat=json',  
+		data: {
+			'method': 'addName',
+			'name': newName,
+			'position': currentPosition,
+			'gender': gender
 			},
 		contentType: 'json',
 		dataType: 'json',
@@ -23,4 +86,56 @@ function addName(){
 			console.log("addName run");
 		}	
 	});
+}
+
+function clearNames(){
+	var gender = "male";
+
+	$.ajax({
+		type: 'get', 
+		url: 'http://www.chesteraustin.us/cfc/TPNames.cfc?ReturnFormat=json',  
+		data: {
+			'method': 'clearGender',
+			'gender': gender
+			},
+		contentType: 'json',
+		dataType: 'json',
+		success: function(response) {
+			console.log("clearNames run");
+		}	
+	});
+
+	//Repull names
+	getNames();
+}
+
+function saveNames(){ 
+	//Empty list
+	clearNames();
+
+	//Add Name
+	$("li").each(function(index){
+		var newName = $(this).text();
+		var gender = "male";
+		var currentPosition = index;
+
+		$.ajax({
+			type: 'get', 
+			url: 'http://www.chesteraustin.us/cfc/TPNames.cfc?ReturnFormat=json',  
+			data: {
+				'method': 'addName',
+				'name': newName,
+				'position': currentPosition,
+				'gender': gender
+				},
+			contentType: 'json',
+			dataType: 'json',
+			success: function(response) {
+				console.log("addName run");
+			}	
+		});
+	});
+
+	//Repull names
+	getNames();
 }
